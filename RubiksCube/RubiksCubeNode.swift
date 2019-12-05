5777266cec0bd4c875e978047b91ae4ea3ba59ad
Logan <Logan.Richards@colorado.edu>
2019-12-04 17:27:40 -0700

    Rotation X, Y, Z

--- RubiksCube/RubiksCubeNode.swift
+++ RubiksCube/RubiksCubeNode.swift
@@ -40,54 +40,116 @@ enum Color {
     }
 }
 
+struct Material {
+	var mats: [Color]
+	
+	func getMat() -> [SCNMaterial] {
+		return [mats[0].mat, mats[1].mat, mats[2].mat, mats[3].mat, mats[4].mat, mats[5].mat]
+	}
+	
+	mutating func rotateX(pos: Bool) {
+		var temp: [Color] = [Color]()
+		
+		for m in mats {
+			temp.append(m)
+		}
+		
+		
+		if(pos) {
+			self.mats[0] = temp[4]
+			self.mats[2] = temp[5]
+			self.mats[4] = temp[2]
+			self.mats[5] = temp[0]
+		} else {
+			self.mats[0] = temp[5]
+			self.mats[2] = temp[4]
+			self.mats[4] = temp[0]
+			self.mats[5] = temp[2]
+		}
+	}
+	
+	mutating func rotateY(pos: Bool) {
+		let temp: [Color] = self.mats
+		
+		if(pos) {
+			self.mats[0] = temp[3]
+			self.mats[1] = temp[0]
+			self.mats[2] = temp[1]
+			self.mats[3] = temp[2]
+		} else {
+			self.mats[0] = temp[1]
+			self.mats[1] = temp[2]
+			self.mats[2] = temp[3]
+			self.mats[3] = temp[0]
+		}
+	}
+
+	mutating func rotateZ(pos: Bool) {
+		let temp: [Color] = self.mats
+		
+		if(pos) {
+			self.mats[1] = temp[5]
+			self.mats[3] = temp[4]
+			self.mats[4] = temp[1]
+			self.mats[5] = temp[3]
+		} else {
+			self.mats[1] = temp[4]
+			self.mats[3] = temp[5]
+			self.mats[4] = temp[3]
+			self.mats[5] = temp[1]
+		}
+	}
+	
+	
+}
+
 class RubiksCubeNode: SCNNode {
 	let size: Int
 	
 	var boxs: [SCNNode]
+	var boxMats: [Material]
 	let sideColors: [Color]
 	let insideColor: Color
 	
 	let deg90: CGFloat = 3.1415926536/2
-	
-	var front: [[Color]] = [[.red, .red, .red],
-							[.red, .red, .red],
-							[.red, .red, .red]]
-	var back: [[Color]] = [[.blue, .blue, .blue],
-						   [.blue, .blue, .blue],
-						   [.blue, .blue, .blue]]
-	var top: [[Color]] = [[.orange, .orange, .orange],
-						  [.orange, .orange, .orange],
-						  [.orange, .orange, .orange]]
-	var bottom: [[Color]] = [[.white, .white, .white],
-							 [.white, .white, .white],
-							 [.white, .white, .white]]
-	var left: [[Color]] = [[.yellow, .yellow, .yellow],
-						   [.yellow, .yellow, .yellow],
-						   [.yellow, .yellow, .yellow]]
-	var right: [[Color]] = [[.green, .green, .green],
-							[.green, .green, .green],
-							[.green, .green, .green]]
 	
 	init(size: Int) {
 		self.size = size
 		self.insideColor = Color.black
 		self.sideColors = [Color.red, Color.green, Color.blue, Color.yellow, Color.orange, Color.white]
 		self.boxs = [SCNNode](repeating: SCNNode(), count: size * size * size)
-				
+		self.boxMats = [Material](repeating: Material(mats: [Color.red, Color.green, Color.blue, Color.yellow, Color.orange, Color.white]), count: size * size * size)
+		
 		super.init()
+		
+		setup()
+		resetBrick()
+		rotateBrick()
+	}
+	
+	func setup() {
+		for i in 0..<boxs.count {
+			self.boxMats[i] = Material(mats: self.sideColors)
+		}
+	}
+	
+	func resetBrick() {
+		for node in self.childNodes {
+			node.removeFromParentNode()
+		}
 		
 		for i in 0..<boxs.count {
 			self.boxs[i] = SCNNode()
 			let child: SCNNode = SCNNode()
 			
 			let box = SCNBox(width: 0.98, height: 0.98, length: 0.98, chamferRadius: 0.1)
-			var mat: [SCNMaterial] = [sideColors[0].mat, sideColors[1].mat, sideColors[2].mat, sideColors[3].mat, sideColors[4].mat, sideColors[5].mat]
+			var mat: [SCNMaterial] = self.boxMats[i].getMat()
 			
 			let row = i % size
 			let col = (i / size) % size
 			let height = i / (size * size)
 			
-			
+			/*
 			//Change insides to black
 			if(height != size - 1) {
 				mat[0] = insideColor.mat
@@ -111,36 +173,67 @@ class RubiksCubeNode: SCNNode {
 			
 			if(col != 0) {
 				mat[5] = insideColor.mat
-			}
+			}*/
 			
 			box.materials = mat
 			
 			child.geometry = box
 			self.boxs[i].addChildNode(child)
+			//self.boxs[i] = child
 			child.worldPosition.x = Float(row) - Float(size)/2.0 + 0.5
 			child.worldPosition.y = Float(col) - Float(size)/2.0 + 0.5
 			child.worldPosition.z = Float(height) - Float(size)/2.0 + 0.5
 			
 			self.addChildNode(self.boxs[i])
 		}
-		
-		rotateBrick()
 	}
 	
-	
 	func rotateBrick() {
+		rotateY()
+		rotateX()
+		rotateZ()
+	}
+		
+	func rotateX() {
 		for i in 0..<boxs.count {
 			let row = i % size
-			let col = (i / size) % size
-			let height = i / (size * size)
 			
 			if(row == 0) {
 				let action = SCNAction.rotateBy(x: deg90, y: 0, z: 0, duration: 1)
-				self.boxs[i].runAction(action)
+				self.boxs[i].runAction(action, completionHandler: {
+					self.boxMats[i].rotateX(pos: true)
+					self.resetBrick()
+				})
 			}
 		}
-		
-		
+	}
+	
+	func rotateY() {
+		for i in 0..<boxs.count {
+			let col = (i / size) % size
+			
+			if(col == 0) {
+				let action = SCNAction.rotateBy(x: 0, y: deg90, z: 0, duration: 1)
+				self.boxs[i].runAction(action, completionHandler: {
+					self.boxMats[i].rotateY(pos: true)
+					self.resetBrick()
+				})
+			}
+		}
+	}
+
+	func rotateZ() {
+		for i in 0..<boxs.count {
+			let height = i / (size * size)
+			
+			if(height == 0) {
+				let action = SCNAction.rotateBy(x: 0, y: 0, z: deg90, duration: 1)
+				self.boxs[i].runAction(action, completionHandler: {
+					self.boxMats[i].rotateZ(pos: true)
+					self.resetBrick()
+				})
+			}
+		}
 	}
 	
 	required init?(coder: NSCoder) {
